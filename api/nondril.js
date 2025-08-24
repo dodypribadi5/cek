f19e595c9d5fcc4f76e3d3fbefbbcefd45f110ee
dodypribadi5 <dodypribadi5@gmail.com>
2025-08-24 13:40:40 +0700

    Update nondril.js

--- api/nondril.js
+++ api/nondril.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cek-two.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
